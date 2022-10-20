cfe72cac1f45c12752aa3db1066a9b3effbf0298
poojasingh-legion <115175534+poojasingh-legion@users.noreply.github.com>
2022-10-21 01:58:23 +0530

    basic project setup

--- app/src/main/java/com/poojasingh/exploringandroid/ui/activities/MainActivity.kt
+++ app/src/main/java/com/poojasingh/exploringandroid/ui/activities/MainActivity.kt
@@ -1,11 +1,11 @@
-package com.poojasingh.exploringandroid
+package com.poojasingh.exploringandroid.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
